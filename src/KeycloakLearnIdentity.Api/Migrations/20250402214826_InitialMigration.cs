4b9e1cd3dc25b0e2d508db98acbcdb3a6c9abab8
SzymonKowalski <SzymonKowalski878@gmail.com>
2025-04-02 23:51:34 +0200

    store users in sql, email confirmation endpoint

---
+++ src/KeycloakLearnIdentity.Api/Migrations/20250402214826_InitialMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeycloakLearnIdentity.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    KeycloakId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    IsEmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    ConfirmationToken = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ConfirmationToken",
+                table: "Users",
+                column: "ConfirmationToken",
+                unique: true,
+                filter: "[ConfirmationToken] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_KeycloakId",
+                table: "Users",
+                column: "KeycloakId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- src/KeycloakLearnIdentity.Api/Services/KeycloakService.cs
+++ src/KeycloakLearnIdentity.Api/Services/KeycloakService.cs
@@ -1,8 +1,8 @@
-﻿using Azure.Core;
-using Feree.ResultType;
+﻿using Feree.ResultType;
 using Feree.ResultType.Factories;
 using Feree.ResultType.Results;
 using KeycloakLearnIdentity.Api.Models;
+using KeycloakLearnIdentity.Api.Repositories;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Headers;
 using System.Text;
@@ -16,6 +16,7 @@ public interface IKeycloakService
     Task<IResult<TokensResponse>> RefreshTokens(RefreshTokensRequest refreshRequest);
     Task<IResult<Unit>> Register(RegisterRequest request);
     Task<IResult<List<UserResponse>>> GetUsers();
+    Task<IResult<User>> ConfirmUser(string confirmationToken);
 }
 
 public class KeycloakService : IKeycloakService
@@ -23,14 +24,17 @@ public class KeycloakService : IKeycloakService
     private readonly KeycloakSettings _keycloakSettings;
     private readonly HttpClient _httpClient;
     private readonly ILogger<KeycloakService> _logger;
+    private readonly IUserRepository _userRepository;
 
     public KeycloakService(IOptions<KeycloakSettings> keycloakSettings,
         IHttpClientFactory httpClientFactory,
-        ILogger<KeycloakService> logger)
+        ILogger<KeycloakService> logger,
+        IUserRepository userRepository)
     {
         _keycloakSettings = keycloakSettings.Value;
         _httpClient = httpClientFactory.CreateClient();
         _logger = logger;
+        _userRepository = userRepository;
     }
 
     public async Task<IResult<TokensResponse>> Login(LoginRequest request)
@@ -108,7 +112,21 @@ public class KeycloakService : IKeycloakService
             return ResultFactory.CreateFailure($"Failed to register user. Details: {errorDetails}");
         }
 
-        return ResultFactory.CreateSuccess();
+        var locationHeader = response.Headers.Location;
+        if (locationHeader == null)
+        {
+            return ResultFactory.CreateFailure("Failed to retrieve user ID from Keycloak response.");
+        }
+
+        var userId = locationHeader.Segments.Last();
+
+        var confirmationToken = Guid.NewGuid().ToString();
+
+        var user = new User(userId, request.Email, request.FirstName, request.LastName, request.Email, true, false, confirmationToken);
+
+        return await _userRepository.AddUserAsync(user)
+            .BindAsync(_ => _userRepository.CommitAsync())
+            .BindAsync(_ => SendConfirmationEmailAsync(request.Email, confirmationToken));
     }
 
     public async Task<IResult<List<UserResponse>>> GetUsers()
@@ -196,5 +214,51 @@ public class KeycloakService : IKeycloakService
         }
 
         return ResultFactory.CreateSuccess(deserializedResponse);
+    }
+
+    private  Task<IResult<Unit>> SendConfirmationEmailAsync(string email, string confirmationToken)
+    {
+        //TODO: Implement email sending logic here
+        return ResultFactory.CreateSuccessAsync();
+    }
+
+    public Task<IResult<User>> ConfirmUser(string confirmationToken) =>
+        _userRepository.GetUserByConfirmationTokenAsync(confirmationToken)
+            .BindAsync(user => ConfirmKeycloakUserAsync(user.KeycloakId)
+                .BindAsync(_ => _userRepository.UpdateUserAsync(
+                    user.SetIsEmailConfirmed(true).ResetConfirmationToken())
+                .BindAsync(user => _userRepository.CommitAsync()
+                .BindAsync(_ => ResultFactory.CreateSuccessAsync(user)))));
+
+    private async Task<IResult<Unit>> ConfirmKeycloakUserAsync(string keycloakId)
+    {
+        var tokenResponse = await GetAdminAccessTokenAsync();
+        if (string.IsNullOrEmpty(tokenResponse))
+        {
+            return ResultFactory.CreateFailure<Unit>("Unable to authenticate with Keycloak.");
+        }
+
+        var keycloakAdminUrl = $"{_keycloakSettings.AdminLink}/users/{keycloakId}";
+        var updateUserPayload = new
+        {
+            emailVerified = true
+        };
+        var jsonContent = new StringContent(JsonSerializer.Serialize(updateUserPayload), Encoding.UTF8, "application/json");
+
+        var httpRequest = new HttpRequestMessage(HttpMethod.Put, keycloakAdminUrl)
+        {
+            Headers = { Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse) },
+            Content = jsonContent
+        };
+
+        var response = await _httpClient.SendAsync(httpRequest);
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorDetails = await response.Content.ReadAsStringAsync();
+            _logger.LogError("Failed to confirm Keycloak user. Details: {ErrorDetails}", errorDetails);
+            return ResultFactory.CreateFailure<Unit>($"Failed to confirm Keycloak user. Details: {errorDetails}");
+        }
+
+        return ResultFactory.CreateSuccess();
     }
 }
